555e225df3673517acf886fcb017e9a8056af8ed
sanae <sanae@gmail.com>
2023-03-19 17:25:28 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+#!/bin/bash
 #include <stdio.h>
-	/**
-	* main - A program that prints a line with puts function
-	* Return : 0 (Succes)
-	*/
-	int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-	}
+/**
+* main - A program that prints a line with puts function
+* Return : 0 (Succes)
+*/
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 /**
 * main - Prints out a string to stdout.
