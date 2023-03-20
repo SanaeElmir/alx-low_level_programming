51bfbd38976db4002d4ef87e34dfc068492ec51d
sanae <sanae@gmail.com>
2023-03-20 07:15:40 -0700

    Task 5, Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - Prints out a string to stdout.
-* Return : 0 (Succes)
+* Return: 0 (Succes)
 */
 int main(void) {
 printf("with proper grammar, but the outcome is a piece of art,\n");
