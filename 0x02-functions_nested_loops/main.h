32680d07a49847a8d681249272c7ec34dd8e7e6c
sanae <sanae@gmail.com>
2023-03-22 10:00:43 -0700

    Hello World

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,13 @@
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void islower(int c);
+void isalpha(int c);
+void print_sign(int n);
+void _abs(int);
+void print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
